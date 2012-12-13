af40ca859a717de65c71f1edf4a6a409ae9c6d8f
David Makin <sleep-er@sleep-er.co.uk>
2012-12-13 16:49:43 +0000

    added a phpunit test and autoloader

---
+++ classes/Example.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Example class
+ */
+class Example
+{
+
+    public $intNumberOne;
+
+    /**
+     * Class constructor
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function setIntNumberOne($newValue)
+    {
+        $this->intNumberOne = $newValue;
+    }
+}
